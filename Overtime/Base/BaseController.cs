1bd77438fe5bb7516f7e0bf02b5fac8ed8938eda
Kllev <54259977+Kllev@users.noreply.github.com>
2021-10-04 20:16:09 +0700

    new

---
+++ Overtime/Base/BaseController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Overtime.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Overtime.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController<Entity, Repository, Key> : ControllerBase
+        where Entity : class
+        where Repository : IRepository<Entity, Key>
+    {
+        private readonly Repository repository;
+        public BaseController(Repository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpPost]
+        public ActionResult Insert(Entity entity)
+        {
+            try
+            {
+                if (repository.Insert(entity) > 0)
+                {
+                    return Ok(new
+                    {
+                        status = HttpStatusCode.OK,
+                        //data = personRepository.Get(person.NIK),
+                        message = "Data berhasil Di Tambahkan"
+                    });
+                }
+                else if (repository.Insert(entity) == 0)
+                {
+                    return BadRequest(new
+                    {
+                        status = HttpStatusCode.BadRequest,
+                        message = "NIK tidak boleh kosong"
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return BadRequest(new
+            {
+                status = HttpStatusCode.BadRequest,
+                message = "Data Sudah Ada"
+            });
+        }
+
+        [HttpGet]
+        public ActionResult<Entity> Get()
+        {
+            if (repository.Get() != null)
+            {
+
+                return Ok(new
+                {
+                    status = HttpStatusCode.OK,
+                    data = repository.Get(),
+                    message = "Data berhasil Di tampilkan"
+                });
+            }
+            else
+            {
+                return NotFound("Data Tidak Ada");
+            }
+        }
+
+        [HttpGet("{key}")]
+        public ActionResult Get(Key key)
+        {
+            if (repository.Get(key) != null)
+            {
+
+                //return Ok(personRepository.Get(NIK));
+                return Ok(new
+                {
+                    status = HttpStatusCode.OK,
+                    data = repository.Get(key),
+                    message = "Data berhasil Di tampilkan"
+                });
+            }
+            return NotFound(new
+            {
+                status = HttpStatusCode.NotFound,
+                message = "Data dengan NIK tersebut Tidak Ditemukan"
+                //return Ok(personRepository.Get(NIK));
+            });
+        }
+
+        [HttpPut]
+        public ActionResult Update(Entity entity)
+        {
+            try
+            {
+                if (repository.Update(entity) != 0)
+                {
+                    //return Ok("Data Berhasil di Update");
+                    return Ok(new
+                    {
+                        status = HttpStatusCode.OK,
+                        //data = repository.Get(entity.key),
+                        message = "Data berhasil Di Update"
+                    });
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return NotFound(new
+            {
+                status = HttpStatusCode.NotFound,
+                message = "Data dengan NIK tersebut Tidak Ditemukan"
+
+            });
+        }
+
+        [HttpDelete("{key}")]
+        public ActionResult Delete(Key key)
+        {
+            if (repository.Get(key) != null)
+            {
+                //personRepository.Delete(NIK);
+                //return Ok("Data Berhasil Dihapus");  
+                return Ok(new
+                {
+                    status = HttpStatusCode.OK,
+                    data = repository.Get(key),
+                    deletedata = repository.Delete(key),
+                    message = "Data berhasil Di Hapus"
+                });
+            }
+            else
+            {
+                return BadRequest(new
+                {
+                    status = HttpStatusCode.NotFound,
+                    message = "Data dengan NIK tersebut Tidak Ditemukan"
+                });
+            }
+        }
+    }
+}
